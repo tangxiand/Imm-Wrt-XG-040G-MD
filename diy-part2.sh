2f71b557c527dc628c80d20f373d4110d9bccbdd
tangxiand <197554977@qq.com>
2026-03-05 09:48:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+#!/bin/bash
+
+# 安装 iStore 相关包
+./scripts/feeds install -a -p istore
+
+# （可选）修改默认 IP、主机名等
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt-XG040G-MD/g' package/base-files/files/bin/config_generate
